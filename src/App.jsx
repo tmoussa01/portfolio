f01994cc5c666329c22955b597592a52ceea69ba
tahri moussa <tamo1004@student.iepscf-uccle.be>
2024-06-11 01:28:44 +0200

    hidden scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,25 @@ import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Skills from "./components/skill/Skill";
 
-
-
 const App = () => {
-  return <div>
-    <section id="Homepage">
-      <Navbar />
-      <Hero />
-
-    </section>
-    <section id="Skills"><Parallax type="skills" /></section>
-    <section>
-      <Skills />
-    </section>
-    <section id="Portfolio"><Parallax type="portfolio" /></section>
-    <section id="Contact"></section>
-
-
-
-
-
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Skills">
+        <Parallax type="skills" />
+      </section>
+      <section>
+        <Skills />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section id="Contact"></section>
+    </div>
+  );
 };
 
 export default App;
